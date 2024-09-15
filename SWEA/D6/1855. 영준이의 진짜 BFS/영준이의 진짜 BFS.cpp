e8f100300313bb40c7826bf5034ce3f8feb166f1
bit_generator <53938713+cellcluster@users.noreply.github.com>
2024-09-15 23:46:49 +0900

    Update 영준이의 진짜 BFS.cpp

--- SWEA/D6/1855. 영준이의 진짜 BFS/영준이의 진짜 BFS.cpp
+++ SWEA/D6/1855. 영준이의 진짜 BFS/영준이의 진짜 BFS.cpp
@@ -9,75 +9,55 @@ int T, N, P;
 long long ans;
 int depth[100001];
 int LCA[100001][18];
+vector<int> order;
 
 void init(vector<vector<int>>& graph) {
-    queue<pair<int, int>> q;
-    q.emplace(1, 0);
+    queue<int> q;
+    q.push(1);
+    order.clear();
+    ans = 0;
     
     while (!q.empty()) {
-        int cur = q.front().first;
-        int prv = q.front().second;
+        int cur = q.front();
         q.pop();
+        order.push_back(cur);
 
         for (int nxt : graph[cur]) {
-            if (nxt == prv) continue;
-
             for (int i = 1; i < 18; i++) {
                 LCA[nxt][i] = LCA[LCA[nxt][i - 1]][i - 1];
             }
 
             depth[nxt] = depth[cur] + 1;
-            q.emplace(nxt, cur);
+            q.push(nxt);
         }
     }
 }
 
-int getDistance(int a, int b) {
+int getLCA(int a, int b) {
     if (depth[a] < depth[b]) swap(a, b);
-
-    int ret = 0;
 
     if (depth[a] != depth[b]) {
         for (int i = 17; i >= 0; i--) {
-            if (depth[a] - (1 << i) >= depth[b]) {
+            if (depth[LCA[a][i]] >= depth[b]) {
                 a = LCA[a][i];
-                ret += (1 << i);
             }
         }
     }
     
-    if (a == b) return ret;
+    if (a == b) return a;
+
+    int ret = 0;
 
     for (int i = 17; i >= 0; i--) {
         if (LCA[a][i] != LCA[b][i]) {
             a = LCA[a][i];
             b = LCA[b][i];
-            ret += (2 * (1 << i));
         }
+
+        ret = LCA[a][i];
     }
     
-    ret += 2;
     return ret;
-}
-
-void bfs(vector<vector<int>>& graph) {
-    queue<pair<int, int>> q;
-    q.emplace(1, 0);
-    int prvNode = 1;
-    
-    while (!q.empty()) {
-        int cur = q.front().first;
-        int prev = q.front().second;
-        q.pop();
-
-        for (int nxt : graph[cur]) {
-            if (nxt == prev) continue;
-
-            ans += getDistance(prvNode, nxt);
-            q.emplace(nxt, cur);
-            prvNode = nxt;
-        }
-    }
 }
 
 int main() {
@@ -89,17 +69,18 @@ int main() {
         cin >> N;
 
         vector<vector<int>> graph(100001);
-        ans = 0;
 
         for (int i = 2; i <= N; i++) {
             cin >> P;
             LCA[i][0] = P;
-            graph[i].push_back(P);
             graph[P].push_back(i);
         }
 
         init(graph);
-        bfs(graph);
+        
+        for (int i = 1; i < order.size(); i++) {
+            ans += (depth[order[i - 1]] + depth[order[i]] - 2 * depth[getLCA(order[i - 1], order[i])]);
+        }
 
         cout << '#' << tc << ' ' << ans << '\n';
     }
