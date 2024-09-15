feb1dc930887c577078bf1c67e797bd4cf53479c
bit_generator <53938713+cellcluster@users.noreply.github.com>
2024-09-15 18:57:09 +0900

    [Platinum V] Title: 이진 검색 트리, Time: 136 ms, Memory: 16704 KB -BaekjoonHub

---
+++ 백준/Platinum/1539. 이진 검색 트리/이진 검색 트리.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#define LEFT    0
+#define RIGHT    1
+#define EMPTY    -1
+using namespace std;
+
+int N, num;
+long long ans;
+set<int> s;
+int tree[250002][2];
+int depth[250002];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int i = 0; i <= 250001; i++) {
+        fill(tree[i], tree[i] + 2, EMPTY);
+    }
+
+    cin >> N;
+    s.insert(250001);
+
+    while (N--) {
+        cin >> num;
+        int parent = *(s.lower_bound(num));
+        if (tree[parent][LEFT] == EMPTY) {
+            tree[parent][LEFT] = num;
+            depth[num] = depth[parent] + 1;
+        }
+        else {
+            parent = tree[parent][LEFT];
+            if (tree[parent][RIGHT] == EMPTY) {
+                tree[parent][RIGHT] = num;
+                depth[num] = depth[parent] + 1;
+            }
+            else {
+                tree[tree[parent][RIGHT]][RIGHT] = num;
+                depth[num] = depth[tree[parent][RIGHT]] + 1;
+                tree[parent][RIGHT] = num;
+            }
+        }
+
+        ans += depth[num];
+        s.insert(num);
+    }
+
+    cout << ans;
+    return 0;
+}
