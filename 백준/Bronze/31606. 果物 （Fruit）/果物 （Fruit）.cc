2f687ca1fcad662776da04de4086ac66698156d4
bit-generator <53938713+bit-generator@users.noreply.github.com>
2024-11-13 00:03:20 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int X, Y;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> X >> Y;
+    cout << X + Y + 3;
+    return 0;
+}
