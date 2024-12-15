1a44ac2131a76c36f911639ad03d1828db2e763a
bit-generator <53938713+bit-generator@users.noreply.github.com>
2024-12-16 00:42:21 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "       _.-;;-._\n";
+	cout << "\'-..-\'|   ||   |\n";
+	cout << "\'-..-\'|_.-;;-._|\n";
+	cout << "\'-..-\'|   ||   |\n";
+	cout << "\'-..-\'|_.-\'\'-._|";
+
+	return 0;
+}
