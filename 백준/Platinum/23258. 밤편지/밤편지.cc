6c56c2f6b092ee05a046ce6da925d3f1336cf56e
bit-generator <53938713+bit-generator@users.noreply.github.com>
2025-06-04 16:01:22 +0900

    [Platinum V] Title: 밤편지, Time: 284 ms, Memory: 108900 KB -BaekjoonHub

---
+++ 백준/Platinum/23258. 밤편지/밤편지.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int N, Q, C, s, e;
+int graph[302][301][301];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> Q;
+    for(int i = 1; i <= N; ++i) {
+        for(int j = 1; j <= N; ++j) {
+            cin >> graph[1][i][j];
+        }
+    }
+    
+    for(int k = 1; k <= N; ++k) {
+        for(int i = 1; i <= N; ++i) {
+            for(int j = 1; j <= N; ++j) {
+                if(i == j || !graph[i][k] || !graph[k][j]) {
+                    graph[k + 1][i][j] = graph[k][i][j];
+                    continue;
+                }
+                
+                if(!graph[k][i][j]) graph[k + 1][i][j] = graph[k][i][k] + graph[k][k][j];
+                else graph[k + 1][i][j] = min(graph[k][i][j], graph[k][i][k] + graph[k][k][j]);
+            }
+        }
+    }
+    
+    while(Q--) {
+        cin >> C >> s >> e;
+        if(graph[C][s][e] || s == e) cout << graph[C][s][e] << '\n';
+        else cout << -1 << '\n';
+    }
+    
+    return 0;
+}
