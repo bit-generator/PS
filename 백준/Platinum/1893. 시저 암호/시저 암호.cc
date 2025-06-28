7de08ed8c72e025869a2c17df18fc06ad9bdec25
bit-generator <53938713+bit-generator@users.noreply.github.com>
2025-06-29 03:33:59 +0900

    [Platinum IV] Title: 시저 암호, Time: 252 ms, Memory: 3300 KB -BaekjoonHub

--- 백준/Platinum/1893. 시저 암호/시저 암호.cc
+++ 백준/Platinum/1893. 시저 암호/시저 암호.cc
@@ -7,8 +7,7 @@ using namespace std;
 int N;
 string A, W, S;
 int F[50001];
-int idx[62];
-int idxmap[128];
+int nxtIdx[128];
 int cnt[62];
 vector<int> ans;
 
@@ -21,10 +20,9 @@ int main() {
         cin >> A >> W >> S;
 
         for (int i = 0; i < A.length(); ++i) {
-            idxmap[A[i]] = i;
+            nxtIdx[A[i]] = (i + 1) % A.length();
         }
         memset(F, 0, sizeof(F));
-        memset(idx, 0, sizeof(idx));
         memset(cnt, 0, sizeof(cnt));
         ans.clear();
 
@@ -34,21 +32,23 @@ int main() {
             if (W[i] == W[j]) F[i] = ++j;
         }
 
-        for (int i = 0; i < S.length(); ++i) {
-            for (j = 0; j < A.length(); ++j) {
-                int& cur = idx[j];
-                char cmp = A[(idxmap[W[cur]] + j) % A.length()];
-                while (cur > 0 && S[i] != cmp) {
-                    cur = F[cur - 1];
-					cmp = A[(idxmap[W[cur]] + j) % A.length()];
-                }
-
-                if (S[i] == cmp) ++cur;
-                if (cur == W.length()) {
-                    ++cnt[j];
-                    cur = F[cur - 1];
+        for(int i = 0; i < A.length(); ++i) {
+            if(i > 0) {
+                for(j = 0; j < W.length(); ++j) {
+                    W[j] = A[nxtIdx[W[j]]];
                 }
             }
+            
+            int idx = 0;
+            for(j = 0; j < S.length(); ++j) {
+                while(idx > 0 && S[j] != W[idx]) idx = F[idx - 1];
+                if(S[j] == W[idx]) ++idx;
+                if(idx == W.length()) {
+                    ++cnt[i];
+                    idx = F[idx - 1];
+                }
+            }
+            
         }
 
         for (int i = 0; i < A.length(); ++i) {
