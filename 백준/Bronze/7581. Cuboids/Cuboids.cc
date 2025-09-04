dc315e7bb22cb83c4b47ff03522fa2c215ad6153
bit-generator <53938713+bit-generator@users.noreply.github.com>
2025-09-05 01:55:37 +0900

    [Bronze III] Title: Cuboids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int l, w, h, v;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    while(true) {
+        cin >> l >> w >> h >> v;
+        if(l == 0 && w == 0 && h == 0 && v == 0) break;
+        
+        if(l == 0) l = v / w / h;
+        else if(w == 0) w = v / l / h;
+        else if(h == 0) h = v / l / w;
+        else if(v == 0) v = l * w * h;
+        
+        cout << l << ' ' << w << ' ' << h << ' ' << v << '\n';
+    }
+    
+    return 0;
+}
