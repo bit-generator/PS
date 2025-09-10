031aa4665cdef50b0fa4da23ae0085c3489d38d5
bit-generator <53938713+bit-generator@users.noreply.github.com>
2025-09-11 01:22:26 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> M;
+    if(N * 100 >= M) cout << "Yes";
+    else cout << "No";
+    
+    return 0;
+}
