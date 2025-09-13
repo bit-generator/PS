b3ef5dfa7fd83937d79e081966a0a249142f37db
bit-generator <53938713+bit-generator@users.noreply.github.com>
2025-09-13 19:20:17 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[3];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr, arr + 3);
+    cout << arr[1];
+    return 0;
+}
