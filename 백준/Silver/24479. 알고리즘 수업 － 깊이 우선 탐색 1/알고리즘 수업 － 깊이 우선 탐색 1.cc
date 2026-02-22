9cc98ddb41210ae71672458460d4d0fcd2ab67a6
bit-generator <53938713+bit-generator@users.noreply.github.com>
2026-02-22 20:58:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 228 ms, Memory: 26516 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int N, M, R, u, v, orderCnt;
+set<int> graph[100001];
+int order[100001];
+
+void dfs(int cur) {
+    order[cur] = ++orderCnt;
+    
+    for(int nxt : graph[cur]) {
+        if(order[nxt]) continue;
+        dfs(nxt);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> M >> R;
+    while(M--) {
+        cin >> u >> v;
+        graph[u].insert(v);
+        graph[v].insert(u);
+    }
+    
+    dfs(R);
+    for(int i = 1; i <= N; ++i) {
+        cout << order[i] << '\n';
+    }
+    return 0;
+}
