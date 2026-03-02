a4d2ecd73df6c639a48936f80f19db5d9b1ee596
bit-generator <53938713+bit-generator@users.noreply.github.com>
2026-03-03 01:18:11 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, K;
+vector<int> v;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N >> K;
+    for(int i = 1; i <= N; ++i) {
+        if(N % i == 0) v.push_back(i);
+    }
+    
+    if(v.size() < K) cout << 0;
+    else cout << v[K - 1];
+    return 0;
+}
