8b54549df16b7b4db70eb5dd5a9b0970ffd9b60c
bit-generator <53938713+bit-generator@users.noreply.github.com>
2026-03-11 01:51:25 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -9,10 +9,6 @@ int main() {
     
     cin >> a1 >> a0 >> c >> n0;
     if(a1 > c) cout << 0;
-    else if(a1 == c) {
-        if(a0 <= 0) cout << 1;
-        else cout << 0;
-    }
     else {
         if(a0 <= n0 * (c - a1)) cout << 1;
         else cout << 0;
