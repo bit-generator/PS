a9c54e30740ae5b5724584e8b4d317030faf8e79
bit-generator <53938713+bit-generator@users.noreply.github.com>
2026-03-29 21:26:25 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -2,8 +2,19 @@
 #include <string>
 using namespace std;
 
-int T;
+int T, cnt;
 string str;
+
+int recursion(const string& s, int l, int r) {
+    ++cnt;
+    if(l >= r) return 1;
+    else if(s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1);
+}
+
+int isPalindrome(const string& s){
+    return recursion(s, 0, s.length() - 1);
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -12,18 +23,8 @@ int main() {
     cin >> T;
     while(T--) {
         cin >> str;
-        int cnt = 0, len = str.length();
-        bool isPalindrome = true;
-        
-        for(int i = 0; i <= len / 2; ++i) {
-            ++cnt;
-            if(str[i] != str[len - i - 1]) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        
-        cout << isPalindrome << ' ' << cnt << '\n';
+        cnt = 0;
+        cout << isPalindrome(str) << ' ' << cnt << '\n';
     }
     
     return 0;
