53bed1379d781590647458b122445675cc62251f
bit-generator <53938713+bit-generator@users.noreply.github.com>
2026-04-17 01:58:17 +0900

    [Silver III] Title: 카드 숫자 곱의 경우의 수, Time: 88 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/25370. 카드 숫자 곱의 경우의 수/카드 숫자 곱의 경우의 수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int n;
+int arr[7];
+unordered_set<int> us;
+
+void backtrack(int cnt) {
+    if(cnt >= n) {
+        int times = 1;
+        for(int i = 0; i < n; ++i) {
+            times *= arr[i];
+        }
+        
+        us.insert(times);
+        return;
+    }
+    
+    for(int i = 1; i <= 9; ++i) {
+        arr[cnt] = i;
+        backtrack(cnt + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> n;
+    backtrack(0);
+    cout << us.size();
+    return 0;
+}
