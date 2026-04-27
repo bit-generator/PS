7052afef5e087ae62d3c4ddc383f41b82e0753a1
bit-generator <53938713+bit-generator@users.noreply.github.com>
2026-04-27 23:55:19 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> N;
+    for(int i = 0; i < N; ++i) {
+        cout << "I love DGU\n";
+    }
+    
+    return 0;
+}
